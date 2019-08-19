b4765256f25a9075ea19e4f81b8721fda721b0d3
WheatBeer <sungminryu94@gmail.com>
2019-08-19 15:13:28 +0900

    mask weights fractional

---
+++ mask_frac.py
@@ -0,0 +1,49 @@
+import argparse
+import struct
+import torch
+import torch.nn as nn
+from utils.mobilenetv2 import MobileNetV2
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser(description='Mask Weights(FP32) Fractional')
+
+parser.add_argument('-n', '--n-digits', type=int, metavar='#',
+                    default=5, help='Fractional bits | Default: 5')
+
+parser.add_argument('-w', '--weights', metavar='PATH',
+                    default='./data/mobilenet_v2-b0353104.pth',
+                    help='Pretrained parameters PATH | Default: ./data/mobilenet_v2-b0353104.pth')
+
+def masked_fp32(num, n_digits):
+    string_bin = format(struct.unpack('!I', struct.pack('!f', num))[0], '032b')
+    masked = string_bin[:9+n_digits] + '0'*(23-n_digits)
+    return struct.unpack('!f',struct.pack('!I', int(masked, 2)))[0]
+
+def main():
+	args = parser.parse_args()
+
+	model = MobileNetV2()
+	model.load_state_dict(torch.load(args.weights))
+	model.eval()
+	
+	for i, param in enumerate(tqdm(model.parameters())):
+    	if len(param.shape) == 1:
+        	for w in range(param.shape[0]):
+            	param[w] = masked_fp32(param[w], args.n_digits)
+    	if len(param.shape) == 2:
+        	for w in range(param.shape[0]):
+            	for x in range(param.shape[1]):
+                	param[w][x] = masked_fp32(param[w][x], args.n_digits)
+    	if len(param.shape) == 4:
+        	for w in range(param.shape[0]):
+            	for x in range(param.shape[1]):
+                	for y in range(param.shape[2]):
+                    	for z in range(param.shape[3]):
+                        	param[w][x][y][z] = masked_fp32(param[w][x][y][z], args.n_digits)
+	
+	save_file_name = './data/masked' + str(args.n_digits) + '.pth'
+	torch.save(model.state_dict(), save_file_name)
+
+if __name__ == '__main__':
+    main()
+	
